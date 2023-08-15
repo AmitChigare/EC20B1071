8a7f902c29e18b2719b9d48d2b8067ba2d6e243e
AmitChigare <amit.gokakfalls17@gmail.com>
2023-08-15 17:24:55 +0530

    Changed some styling

--- Question 2/train-schedule-app/src/SingleTrain.js
+++ Question 2/train-schedule-app/src/SingleTrain.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import SingleTrainCard from "./SingleTrainCard";
 import axios from "axios";
 
 const SingleTrain = () => {
   const { trainNumber } = useParams();
   const [singleTrain, setSingleTrain] = useState(null);
   const authToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTIwOTgzODEsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiMDA5NTI3YzYtYmE4Ny00NTU4LTk4YjEtNGZjNDEwYjAzNTNmIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IkVDMjBCMTA3MSJ9.HBD732XYcc6CGqQh-_0FqQ8QzOAWxlswaKVQnUE2xag"; // Replace with your actual Bearer token
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTIxMDA0MjgsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiMDA5NTI3YzYtYmE4Ny00NTU4LTk4YjEtNGZjNDEwYjAzNTNmIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IkVDMjBCMTA3MSJ9.6mGqFGsHC2trhSYnaTmL6rN2edZjF8ylpuhvCtHa8X0";
 
   useEffect(() => {
-    // Fetch single train data from the API with authorization header
     axios
       .get(`http://20.244.56.144/train/trains/${trainNumber}`, {
         headers: {
@@ -29,11 +27,29 @@ const SingleTrain = () => {
     <div className="container">
       <h1 className="mt-4">Single Train Details</h1>
       {singleTrain ? (
-        <div>
-          <SingleTrainCard train={singleTrain} />
-          <Link to="/" className="btn btn-secondary mt-3">
-            Back to All Trains
-          </Link>
+        <div className="card mt-4">
+          <div className="card-body">
+            <h5 className="card-title">{singleTrain.trainName}</h5>
+            <p className="card-text">
+              Train Number: {singleTrain.trainNumber}
+              <br />
+              Departure Time: {singleTrain.departureTime.Hours}:
+              {singleTrain.departureTime.Minutes}
+              <br />
+              Seats Available (Sleeper): {singleTrain.seatsAvailable.sleeper}
+              <br />
+              Seats Available (AC): {singleTrain.seatsAvailable.AC}
+              <br />
+              Price (Sleeper): {singleTrain.price.sleeper}
+              <br />
+              Price (AC): {singleTrain.price.AC}
+              <br />
+              Delay (Minutes): {singleTrain.delayedBy}
+            </p>
+            <Link to="/" className="btn btn-secondary">
+              Back to All Trains
+            </Link>
+          </div>
         </div>
       ) : (
         <p>Train not found</p>
--- Question 2/train-schedule-app/src/TrainSchedule.js
+++ Question 2/train-schedule-app/src/TrainSchedule.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
-import TrainCard from "./TrainCard";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const TrainSchedule = () => {
   const [allTrainData, setAllTrainData] = useState([]);
   const authToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTIwOTgzODEsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiMDA5NTI3YzYtYmE4Ny00NTU4LTk4YjEtNGZjNDEwYjAzNTNmIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IkVDMjBCMTA3MSJ9.HBD732XYcc6CGqQh-_0FqQ8QzOAWxlswaKVQnUE2xag"; // Replace with your actual Bearer token
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTIxMDA0MjgsImNvbXBhbnlOYW1lIjoiVHJhaW4gQ2VudHJhbCIsImNsaWVudElEIjoiMDA5NTI3YzYtYmE4Ny00NTU4LTk4YjEtNGZjNDEwYjAzNTNmIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IkVDMjBCMTA3MSJ9.6mGqFGsHC2trhSYnaTmL6rN2edZjF8ylpuhvCtHa8X0";
 
   useEffect(() => {
-    // Fetch train data from the API with authorization header
     axios
       .get("http://20.244.56.144/train/trains", {
         headers: {
@@ -17,20 +16,57 @@ const TrainSchedule = () => {
       })
       .then((response) => {
         setAllTrainData(response.data);
+        console.log(allTrainData);
       })
       .catch((error) => {
         console.error("Error fetching train data:", error);
       });
   }, []);
 
+  console.log(allTrainData);
+
   return (
     <div className="container">
       <h1 className="mt-4">All Trains Schedule</h1>
-      <div className="row mt-4">
-        {allTrainData.map((train, index) => (
-          <TrainCard key={index} train={train} />
-        ))}
-      </div>
+      <table className="table mt-4">
+        <thead>
+          <tr>
+            <th>Train Name</th>
+            <th>Train Number</th>
+            <th>Departure Time</th>
+            <th>Seats Available (Sleeper)</th>
+            <th>Seats Available (AC)</th>
+            <th>Price (Sleeper)</th>
+            <th>Price (AC)</th>
+            <th>Delay (Minutes)</th>
+            <th>View Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allTrainData.map((train, index) => (
+            <tr key={index}>
+              <td>{train.trainName}</td>
+              <td>{train.trainNumber}</td>
+              <td>
+                {train.departureTime.Hours}:{train.departureTime.Minutes}
+              </td>
+              <td>{train.seatsAvailable.sleeper}</td>
+              <td>{train.seatsAvailable.AC}</td>
+              <td>{train.price.sleeper}</td>
+              <td>{train.price.AC}</td>
+              <td>{train.delayedBy}</td>
+              <td>
+                <Link
+                  to={`/single-train/${train.trainNumber}`}
+                  className="btn btn-primary"
+                >
+                  View Details
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
